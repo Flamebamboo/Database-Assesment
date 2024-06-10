00a302738cb772726160e366754fd08e4e9b9376
Flamebamboo <166450271+Flamebamboo@users.noreply.github.com>
2024-06-10 13:51:36 +1200

    Added add_data functions

--- BookDataBase.py
+++ BookDataBase.py
@@ -17,7 +17,7 @@ print(results)
 db.close()
 #All functions for users to input
 #using mixed of tabulate format of "rouded_grid" and fancy_grid
-#1
+
 def print_all_books():
     '''print all books '''
     import sqlite3
@@ -26,8 +26,10 @@ def print_all_books():
     sql = "SELECT * from books;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print(tabulate(results, headers=["ID","Title", "Author", "Publishing Date", "Genre", "Summary"], tablefmt="rounded_grid"))
+    print(tabulate(results, headers=["ID", "Title", "Author", "Publishing Date", "Genre", "Summary"], tablefmt="rounded_grid"))
+
     db.close()
+
 #2
 def print_oldest_books():
     import sqlite3
@@ -46,7 +48,8 @@ def print_newest_books():
     sql = "SELECT * From Books ORDER BY publishing_date DESC;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print(tabulate(results, headers=["ID","Title", "Author", "Publishing Date", "Genre", "Summary"], tablefmt="rounded_grid"))
+    print(tabulate(results, headers=["ID", "Title", "Author", "Publishing Date", "Genre", "Summary"], tablefmt="rounded_grid",))
+
     db.close()
 
 
@@ -103,31 +106,64 @@ def list_all_author():
     print(tabulate(results, headers=["Name"], tablefmt="rounded_grid"))
     db.close()
 
+def add_data():
+    """Add a new book to the database."""
+    import sqlite3
+    db = sqlite3.connect("books.db")
+    cursor = db.cursor()
 
+    book_title = input("Enter book title: ")
+    author_name = input("Enter author name: ")
+    publishing_date = input("Enter publishing date (Year-month-day): ")
+    genre = input("Enter genre: ")
+    summary = input("Enter summary: ")    
+      
+    sql = "INSERT INTO Books (book_name, author_name, publishing_date, genre_type, summary) VALUES (?, ?, ?, ?, ?);"
+    cursor.execute(sql, (book_title, author_name, publishing_date, genre, summary))
+    db.commit()
+
+def remove_data():
+    '''Removing data from the current database'''
+    book_id = input("Enter the ID of the book to remove")
+
+    sql = "DELETE FROM Books WHERE id = ?;"
+    cursor.execute(sql, (book_id,))
+    db.commit()
 
 #main
 while True:
-    user_input = input("What would you like to do. \n1.All books.\n2.Oldest books.\n3.Newest to oldest book.\n4.Oldest to Newest book.\n5.Print childrens literature.\n6.Print fantasy genre.\n7.print science fiction.\n8.list all author.\n9.Exit.\nPrompt here: ")
+
+    #giving the user options to choose from
+    user_input = input("What would you like to do. \n1.All books.\n2.Oldest books.\n3.Newest to oldest book.\n4.Oldest to Newest book.\n5.Print childrens literature.\n6.Print fantasy genre.\n7.print science fiction.\n8.list all author.\n9.add data.\n10.remove data.\n11.edit.\nPrompt here: ")
+   #printing all book with rounded_grid format
     if user_input == "1":
         print_all_books()
-
+    #printing books according to the publishing date oldest to newest in fancy_grid format
     elif user_input == "2":
         print_oldest_books()
-
+    #printing books according to the publishing date newest to oldest in rounded_grid format
     elif user_input == "3":
         print_newest_books()
-
+    #printing all horror genre sort by decending order in rounded_grid format
     elif user_input == "4":
         print_horror_genre()
-
+    #printing all children literature genre by decending order in fancy_grid format
     elif user_input == "5":
         print_childrens_literature()
-
+    #printing all fantasy genre by decending order in fancy_grid format
     elif user_input == "6":
         print_fantasy_genre()
+    #printing  all science fiction books by decending order in fancy_grid format
     elif user_input == "7":
         print_science_fiction()
+    #listing all author in verticle nicely with rounded_grid format
     elif user_input == "8":
         list_all_author()
     elif user_input == "9":
-        break
+        add_data()
+    elif user_input == "10":
+        remove_data()
+    #  elif user_input == "11":
+    #     edit_data()
+    else:
+        print("That is not an option")
